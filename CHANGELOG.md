c5fd7d88428315bb1ed3ac9b8c45c594537737ec
MMZK1526 <mmzk1526@outlook.com>
2025-08-03 15:20:43 +0200

    feat: drop example and add validateEnvWith

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Revision history for mmzk-env
 
 
-## 0.1.0.0 -- Unreleased
+## 0.1.0.0 -- 2025-08-03
 
 * First version. Released on an unsuspecting world.
--- src/Data/Env.hs
+++ src/Data/Env.hs
@@ -1,29 +1,28 @@
-module Data.Env where
+-- |
+-- Module      : Data.Env
+-- Description : Environment schema validation
+--
+-- This module provides functionality to validate environment variables against
+-- a schema (a type that implements the `EnvSchema` class).
+module Data.Env (EnvSchema(..)) where
 
 import           Control.Monad.IO.Class
 import           Data.Env.ExtractFields
 import           Data.Env.RecordParser
-import           GHC.Generics
 
 -- | Type class for validating environment schemas.
 class (ExtractFields a, RecordParser a) => EnvSchema a where
+  -- | Validate the environment variables against the schema, transforming field
+  -- names from camelCase to UPPER_SNAKE_CASE.
   validateEnv :: MonadIO m => m (Either String a)
   validateEnv = do
     envRaw <- getEnvRawCamelCaseToUpperSnake @a
     return $ parseRecord envRaw
 
--- | Example: Define an environment schema
-data Config = Config
-    { port :: Int
-    , name :: String
-    , mainHost :: String
-    , debug :: Maybe Bool }
-    deriving (Show, Generic, EnvSchema)
-
--- | Run the validation
-main :: IO ()
-main = do
-  errOrEnv <- validateEnv @Config
-  case errOrEnv of
-    Left err  -> putStrLn $ "Validation failed: " ++ err
-    Right cfg -> putStrLn $ "Config loaded successfully: " ++ show cfg
+  -- | Validate the environment variables against the schema, allowing for a
+  -- cusutom transformation function to be applied to the field names to match
+  -- with the environment variable names.
+  validateEnvWith :: MonadIO m => (String -> String) -> m (Either String a)
+  validateEnvWith transform = do
+    envRaw <- getEnvRaw @a transform
+    return $ parseRecord envRaw
